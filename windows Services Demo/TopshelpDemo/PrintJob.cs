eab4ef79a21dc5c2ff8a79567587bdec45620062
chensir <chensir_@hotmail.com>
2017-05-25 20:04:29 +0800

    add quartz demo

---
+++ windows Services Demo/TopshelpDemo/PrintJob.cs
@@ -0,0 +1,21 @@
+﻿using Common.Logging;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopshelpDemo
+{
+    public class PrintJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            ILog log = LogManager.GetLogger(typeof(PrintJob));
+            log.Info(" PrintJob 任务执行成功 ");
+            Console.WriteLine("This Print Job");
+        }
+
+    }
+}
--- windows Services Demo/TopshelpDemo/Program.cs
+++ windows Services Demo/TopshelpDemo/Program.cs
@@ -15,22 +15,36 @@ namespace TopshelpDemo
     {
         static void Main(string[] args)
         {
-            var logCfg = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
-            XmlConfigurator.ConfigureAndWatch(logCfg);
+            //var logCfg = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
+            //XmlConfigurator.ConfigureAndWatch(logCfg);
 
+            //HostFactory.Run(x =>
+            //{
+            //    x.Service<TownCrier>(s =>
+            //    {
+            //        s.ConstructUsing(name => new TownCrier());
+            //        s.WhenStarted(tc => tc.Start());
+            //        s.WhenStopped(tc => tc.Stop());
+            //    });
+            //    x.RunAsLocalSystem();
+
+            //    x.SetDescription("TopshelpDemo Description");
+            //    x.SetDisplayName("TopshelpDemo DisplayName");
+            //    x.SetServiceName("TopshelpDemo ServiceName");
+            //});
+
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
             HostFactory.Run(x =>
             {
-                x.Service<TownCrier>(s =>
-                {
-                    s.ConstructUsing(name => new TownCrier());
-                    s.WhenStarted(tc => tc.Start());
-                    s.WhenStopped(tc => tc.Stop());
-                });
-                x.RunAsLocalSystem();
+                x.UseLog4Net();
 
-                x.SetDescription("TopshelpDemo Description");
-                x.SetDisplayName("TopshelpDemo DisplayName");
-                x.SetServiceName("TopshelpDemo ServiceName");
+                x.Service<ServiceRunner>();
+
+                x.SetDescription("QuartzDemo Description");
+                x.SetDisplayName("QuartzDemo DisplayName");
+                x.SetServiceName("QuartzDemo ServiceName");
+
+                x.EnablePauseAndContinue();
             });
         }
     }
@@ -38,7 +52,7 @@ namespace TopshelpDemo
     public class TownCrier
     {
         private Timer _timer = null;
-        readonly ILog _log = LogManager.GetLogger(typeof(TownCrier));
+        readonly ILog _log = LogManager.GetLogger(typeof(Program));
 
         public TownCrier()
         {
